23f7e81e5b17b50b993c3f0fb16ceae723a4f8dc
Crowley <jcrowley317@gmail.com>
2020-03-18 12:49:58 -0700

    01 OO changes: work on total_price in main References #1

--- main.rb
+++ main.rb
@@ -15,14 +15,20 @@ bikes = [
 ]
 
 total_price = 0
-total_price += bikes[0].weekly_rate
-total_price += bikes[0].tail_pack.cost
-total_price += bikes[1].daily_rate * 7
-total_price += bikes[1].panniers[0].price
-total_price += bikes[1].panniers[1].price
-total_price += bikes[2].weekly_rate
-total_price += bikes[2].luggage.price
-total_price += bikes[3].weekly_rate
-total_price += bikes[3].luggage.price
+
+
+
+#total_price += bikes[0].weekly_rate
+#total_price += bikes[0].tail_pack.cost
+
+#total_price += bikes[1].daily_rate * 7
+#total_price += bikes[1].panniers[0].price
+#total_price += bikes[1].panniers[1].price
+
+#total_price += bikes[2].weekly_rate
+#total_price += bikes[2].luggage.price
+
+#total_price += bikes[3].weekly_rate
+#total_price += bikes[3].luggage.price
 
 puts "Total price: #{total_price}"
